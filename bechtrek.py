ba5520a5c3d74124466a423781b693fbcf69965e
Roni Choudhury <roni.choudhury@kitware.com>
2019-12-01 17:51:26 -0500

    Add functions for extracting script title and lines of dialog

--- bechtrek.py
+++ bechtrek.py
@@ -1,8 +1,45 @@
 from bs4 import BeautifulSoup
+from pprint import pprint
 import sys
+
+from typing import Sequence, Optional
+
+
+def get_title(doc: BeautifulSoup) -> Optional[str]:
+    title_parts = doc.title.string.split('-')
+
+    if len(title_parts) < 2:
+        return None
+
+    return '-'.join(title_parts[1:]).strip()
+
+
+def get_dialog(doc: BeautifulSoup) -> Sequence[str]:
+    elems = doc.select('table font')
+    raw_lines = [item for elem in elems for item in elem.stripped_strings]
+
+    lines = [l.replace('\n', ' ') for l in raw_lines]
+
+    return lines
 
 
 def main() -> int:
+    if len(sys.argv) < 2:
+        print('usage: bechtrek.py <scriptfilehtml>', file=sys.stderr)
+        return 1
+
+    htmlscriptfile = sys.argv[1]
+    with open(htmlscriptfile, encoding='ISO-8859-1') as f:
+        htmlscript = f.read()
+
+    doc = BeautifulSoup(htmlscript, features='html.parser')
+
+    title = get_title(doc)
+    dialog = get_dialog(doc)
+
+    print(get_title(doc))
+    pprint(dialog)
+
     return 0
 
 
--- mypy_stubs/bs4.pyi
+++ mypy_stubs/bs4.pyi
@@ -1,2 +1,11 @@
-class BeautifulSoup:
-    pass
+from typing import Iterable, Iterator
+
+
+class BeautifulSoup(Iterable):
+    title: BeautifulSoup
+    string: str
+    stripped_strings: Iterator[str]
+
+    def __init__(self, f: str, features: str): ...
+    def select(self, css: str) -> BeautifulSoup: ...
+    def __iter__(self) -> Iterator[BeautifulSoup]: ...
