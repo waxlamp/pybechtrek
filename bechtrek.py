c06e0c295f508b3e711d8346664d120174d2a3c1
Roni Choudhury <roni.choudhury@kitware.com>
2019-12-01 20:12:00 -0500

    Implement user edit correction for broken parsing

--- bechtrek.py
+++ bechtrek.py
@@ -1,7 +1,9 @@
 from bs4 import BeautifulSoup
+import os
 import parsec
 from pprint import pprint
 import sys
+import tempfile
 
 import parser
 
@@ -26,6 +28,50 @@ def get_dialog(doc: BeautifulSoup) -> Sequence[str]:
     return lines
 
 
+class StopProcessing(BaseException):
+    pass
+
+
+def edit(line):
+    with tempfile.NamedTemporaryFile(mode='w+') as tmpfile:
+        tmp = tmpfile.file
+        name = tmpfile.name
+
+        print(line, file=tmp)
+        print('', file=tmp)
+        print('##########', file=tmp)
+        print('Edit the first line of this file to correct the parsing error', file=tmp)
+        print('Write just the word \'join\' to join this line of text in its entirety to the previous line.', file=tmp)
+        print('Write just the word \'exit\' to abandon processing this script.', file=tmp)
+        tmp.flush()
+
+        os.system(f'vim {name}')
+
+        with open(name) as f:
+            modified = f.readline()
+
+        return modified.strip()
+
+
+def parse_with_correction(line: str) -> str:
+    # If the line parses just fine, then pass it through.
+    result = parser.raw_line.parse(line)
+    if result:
+        return line
+
+    # Otherwise, give the user a chance to fix the issue.
+    modified = edit(line)
+
+    # If the user issued a command, process it; otherwise, pass the edited line
+    # through (after recurseively ensuring that it parses).
+    if modified == 'exit':
+        raise StopProcessing
+    elif modified == 'join':
+        return f'join:{line}'
+    else:
+        return parse_with_correction(modified)
+
+
 def main() -> int:
     if len(sys.argv) < 2:
         print('usage: bechtrek.py <scriptfilehtml>', file=sys.stderr)
@@ -41,10 +87,17 @@ def main() -> int:
     print(title)
 
     dialog = get_dialog(doc)
+    modified = []
     for line in dialog:
-        result = parser.raw_line.parse(line)
+        try:
+            result = parse_with_correction(line)
+        except StopProcessing:
+            print('ending processing early', file=sys.stderr)
+            return 1
 
-        print(result)
+        modified.append(result)
+
+    pprint(modified)
 
     return 0
 
